0361ce4a3195594339f7a08258f6be716dbcc492
jooheeLee <joooohee@kakao.com>
2020-07-14 11:55:36 +0900

    [ADD] 로딩화면 추가

--- index.js
+++ index.js
@@ -1,70 +1,82 @@
 const extractButton = document.querySelector("#extraction-number");
 const noneNumbers = document.querySelector("#none-numbers");
 const lotoNumbers = document.querySelector(".loto-numbers");
+const loading = document.querySelector(".loading");
 
 const MAX = 45;
 const MIN = 1;
 
-// let lotoList = [];
+function handleClick() {
+    loading.classList.remove("none");
+    let loadIndex = 3;
+    loading.querySelector("span").innerText = loadIndex;
+    const loadeInterval = setInterval(() => {
+        loadIndex--;
+        loading.querySelector("span").innerText = loadIndex;
+        if (loadIndex === 0) {
+            clearInterval(loadeInterval);
+            loading.classList.add("none");
+            
+            showLotoNum();
+        }
+    }, 1000);
+}
 
-function showNumber() {
+function showLotoNum() {
     noneNumbers.classList.add("none");
-    while ( lotoNumbers.hasChildNodes() ) { 
-        lotoNumbers.removeChild( lotoNumbers.firstChild ); 
-    }
 
-    const lotoNums =  getLotoNum();
-    lotoNums.map((x,i) => {
-        if(i === lotoNums.length - 1 ) { 
-            const li = document.createElement('li');
-            li.classList.add("number","operator");
+    while (lotoNumbers.hasChildNodes()) {
+        lotoNumbers.removeChild(lotoNumbers.firstChild);
+    }
+    const lotoNums = getLotoNum();
+    lotoNums.map((x, i) => {
+        if (i === lotoNums.length - 1) {
+            const li = document.createElement("li");
+            li.classList.add("number", "operator");
             li.innerText = "+";
             lotoNumbers.appendChild(li);
         }
         const backColor = getNumberColor(x);
-        const li = document.createElement('li');
+        const li = document.createElement("li");
         li.classList.add("number");
         li.innerText = x;
         li.style.background = backColor;
-        lotoNumbers.appendChild(li);      
-    })
+        lotoNumbers.appendChild(li);
+    });
 }
 
 function getNumberColor(index) {
-   if(index <= 10) {
-      return "#f38f06";
-   }else if(index <= 20) {
-      return "#1563d6";
-   }else if(index <=30) {
-      return "#c23b1f";
-   }else if(index <=40) {
-       return "#575d65";
-   }else {
-       return "#1faa18";
-   }
+    if (index <= 10) {
+        return "#f38f06";
+    } else if (index <= 20) {
+        return "#1563d6";
+    } else if (index <= 30) {
+        return "#c23b1f";
+    } else if (index <= 40) {
+        return "#575d65";
+    } else {
+        return "#1faa18";
+    }
 }
 
 function getLotoNum() {
     const array = [];
-    for(let i=0; array.length<6; i++)
-    {
+    for (let i = 0; array.length < 6; i++) {
         const num = randomNum();
-        if(array.indexOf(num) === -1)
-        {
+        if (array.indexOf(num) === -1) {
             array.push(num);
-        } 
+        }
     }
     return array;
 }
 
 function randomNum() {
-   const num =  Math.floor(Math.random() * MAX) + MIN;
-   return num;
+    const num = Math.floor(Math.random() * MAX) + MIN;
+    return num;
 }
 
 function init() {
-    extractButton.addEventListener("click",showNumber);
-
+    extractButton.addEventListener("click", handleClick);
 }
 
 init();
